9fa24e78ee9e85da8aa8078415ae96a7eef7ebd3
navigator515 <gksrlf515@hanmail.net>
2020-08-30 23:48:58 +0900

    코드정리

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {  createMuiTheme } from '@material-ui/core/styles';
 import { BrowserRouter } from 'react-router-dom';
 
-const theme = createMuiTheme({
-typography: {
-fontFamily: '"Noto Sans KR", serif',
-},
-});
 
 ReactDOM.render(
     <BrowserRouter><App/></BrowserRouter>,
