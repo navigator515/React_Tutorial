f54b8fe9265f5fe97daf4adb1d98e9dcfcc6cfc8
navigator515 <gksrlf515@hanmail.net>
2020-08-31 16:15:46 +0900

    proxy 변경

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import CustomerDelete from './CustomerDelete';
-class Customer extends React.Component{
+class Customer extends Component{
     render(){
         return(
               
--- setupProxy.js
+++ setupProxy.js
@@ -1,5 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use("/api/customers", createProxyMiddleware({ target: "http://localhost:5000", changeOrigin: true}));
+  app.use("/api/customers", createProxyMiddleware({ target: "http://15.165.108.107:5000", changeOrigin: true}));
 };
